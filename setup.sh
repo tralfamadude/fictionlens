24a252d73bcfbe51722198ff6c6444ecf0d5ed08
tralfamadude <tralfamadude@gmail.com>
2024-02-03 14:33:47 -0800

    change python=3.10

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #  source this
-conda create --name fictionlens python=3.11a
+conda create --name fictionlens python=3.10
 conda activate fictionlens
 pip install ebooklib beautifulsoup4
 pip install openai
-pip install -r ./frontend/requirements.txt
 pip install -r ./backend/requirements.txt
-python ./backend/app/engine/generate.py
+python ./backend/app/engine/generate.py    # temporary
+(cd backend ; python main.py  >main.py.out 2>main.py.err & )
