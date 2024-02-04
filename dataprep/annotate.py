e7c0439fe7518375ebdd00b2487c9bde4dd47817
tralfamadude <tralfamadude@gmail.com>
2024-02-03 19:13:56 -0800

    key handling fixes

--- dataprep/annotate.py
+++ dataprep/annotate.py
@@ -4,9 +4,22 @@ Requires env to have var OPENAI_API_KEY
 """
 
 import json
-import openai
+from openai import OpenAI
+
 import os
 import sys
+
+annotation_prompt="""
+In the text quoted below, determine the sentiment as -1,0,1 for bad, neutral, good. Identify the names of persons or places. Format these into a JSON with the keys "sentiment" and "entities". Only give JSON as the response. 
+"""
+
+# Set up OpenAI key
+mykey = os.getenv("OPENAI_API_KEY")
+if len(mykey) == 0:
+    print("OPENAI_API_KEY env var not found")
+    sys.exit(1)
+client = OpenAI(api_key=mykey)
+
 
 def read_jsonl_file(filename):
     with open(filename, 'r') as file:
@@ -19,15 +32,15 @@ def write_jsonl_file(filename, data):
 
 def get_embeddings(data):
     for item in data:
-        response = openai.Embedding.create(input=item['text'])
-        item['embedding'] = response['data'][0]['embedding']
+        response = client.embeddings.create(input=item['text'], model="text-embedding-3-small")
+        item['embedding'] = response.data[0].embedding
     return data
 
 def annotate_with_gpt4(data):
     for item in data:
-        prompt = f"Your custom prompt here with {item['text']}"
-        response = openai.Completion.create(model="text-davinci-003", prompt=prompt)
-        item['annotate'] = response['choices'][0]['text'].strip()
+        prompt = f"{annotation_prompt} \n\"\"\"{item['text']}\"\"\""
+        response = client.completions.create(model="gpt-3.5-turbo", prompt=prompt)  # gpt-3.5-turbo
+        item['annotate'] = response.choices[0].text.strip()
     return data
 
 def main():
@@ -40,12 +53,6 @@ def main():
         print("File does not exist.")
         sys.exit(1)
     
-    # Set up OpenAI key
-    mykey = os.getenv("OPENAI_API_KEY")
-    if len(mykey) == 0:
-        print("OPENAI_API_KEY env var not found")
-        sys.exit(1)
-    openai.api_key = mykey
     data = read_jsonl_file(filename)
     print(f"Processing {filename} for embeddings, {len(data)} paragraphs")
     data = get_embeddings(data)
